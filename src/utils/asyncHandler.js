37d2b788c6b02047fbd581df625946cb394fe42d
unknown <abhisheksindhwal11@gmail.com>
2024-04-17 07:40:18 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+export {asyncHandler}
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>()=>{}
+// const asyncHandler=(func)=>async()=>{}
+
+
+// const asyncHandler=(fn)=>async(req,res,next)=>{     //const asyncHandler=(fn)=>{()=>{}} --> Higher Order function
+//                                         //higher order function --> function can accept function and return a function
+//     try{
+//         await fn(req,res,next)
+
+//     }catch(error){
+//         res.status(err.code||500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
